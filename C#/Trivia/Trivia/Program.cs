ac85e898cee0c13bc9226289a58b1be044685b53
Olsenius <andreas@olsenius.com>
2012-12-01 13:26:53 +0100

    Deleted acceptance test project

--- C#/Trivia/Trivia/Program.cs
+++ C#/Trivia/Trivia/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UglyTrivia;
 
 namespace Trivia
 {
@@ -9,6 +10,31 @@ namespace Trivia
     {
         static void Main(string[] args)
         {
+            var game = new Game();
+            game.add("Player 1");
+            game.add("Player 2");
+            game.add("Player 3");
+            bool notAWinner;
+            Random rand = new Random(1);
+
+            do
+            {
+
+                game.roll(rand.Next(5) + 1);
+
+                if (rand.Next(9) == 7)
+                {
+                    notAWinner = game.wrongAnswer();
+                }
+                else
+                {
+                    notAWinner = game.wasCorrectlyAnswered();
+                }
+
+
+
+            } while (notAWinner);
+            Console.Read();
         }
     }
 }
