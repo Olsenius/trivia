ad64a88176c9b11e42a4ffb2d85b78919142d665
Olsenius <andreas@olsenius.com>
2012-12-02 13:47:29 +0100

    Changed test program to write game output to game.txt and diff with expected.txt

--- C#/Trivia/Trivia.AcceptanceTest/Program.cs
+++ C#/Trivia/Trivia.AcceptanceTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using UglyTrivia;
 
 namespace Trivia.AcceptanceTest
@@ -8,15 +9,47 @@ namespace Trivia.AcceptanceTest
         private static readonly Helper _helper = new Helper();
         static void Main()
         {
-            for (int i = 0; i < 50; i++)
+            var console = Console.Out;
+            FileStream ostrm;
+            StreamWriter writer;
+            try
+            {
+                ostrm = new FileStream("./game.txt", FileMode.Create, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot open game.txt for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
+            Console.SetOut(writer);
+
+            for (int i = 0; i < 100; i++)
             {
                 PlayFakeGame();
+            }
+            writer.Close();
+            ostrm.Close();
+
+            Console.SetOut(console);
+            Console.WriteLine("Wrote output of 100 games to game.txt");
+            if (File.Exists("./expected.txt"))
+            {
+                var expected = File.ReadAllText("expected.txt");
+                var actual = File.ReadAllText("game.txt");
+
+                Console.WriteLine();
+                Console.WriteLine("Diff with expected.txt: " + (actual == expected ? "OK!" : "Failure!!!"));
+            }
+            else
+            {
+                Console.WriteLine("expected.txt missing. Rename game.txt to expected.txt and try again");
             }
         }
 
         private static void PlayFakeGame()
         {
-
             var game = new Game();
             game.add("Andreas");
             game.add("Kjersti");
